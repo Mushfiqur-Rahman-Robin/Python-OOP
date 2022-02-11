4639c0818970331d4f6a31c3b46709640fefdb5f
Mushfiqur Rahman <u1604123@student.cuet.ac.bd>
2022-02-11 11:27:47 +0600

    Flask webapp after adding user authentication.

--- Corey Schafer's Python in-depth tutorial/Flask Tutorial/flaskblog/forms.py
+++ Corey Schafer's Python in-depth tutorial/Flask Tutorial/flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -12,6 +13,20 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('Username already exists! Choose another one.')
+
+    def validate_email(self, email):
+
+        user = User.query.filter_by(email = email.data).first()
+        if user:
+            raise ValidationError('Email already exists! Choose another one.')
+
+        
 
 
 class LoginForm(FlaskForm):
