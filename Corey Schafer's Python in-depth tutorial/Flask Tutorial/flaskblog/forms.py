1b9c0e544cee07a67a5baed47a33ae40a88c43f7
Mushfiqur Rahman <u1604123@student.cuet.ac.bd>
2022-02-11 16:26:57 +0600

    Updating and saving user profile in a Flask webapp.

--- Corey Schafer's Python in-depth tutorial/Flask Tutorial/flaskblog/forms.py
+++ Corey Schafer's Python in-depth tutorial/Flask Tutorial/flaskblog/forms.py
@@ -2,7 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
-
+from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
+import secrets
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -35,3 +37,27 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user:
+                raise ValidationError('Username already exists! Choose another one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError('Email already exists! Choose another one.')
