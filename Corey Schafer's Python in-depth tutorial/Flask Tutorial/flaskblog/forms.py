c33a84d68bca30ca3e48af51d50dadf93c498a40
Mushfiqur Rahman <u1604123@student.cuet.ac.bd>
2022-02-12 09:12:49 +0600

    Flask webapp part-9: pagination

--- Corey Schafer's Python in-depth tutorial/Flask Tutorial/flaskblog/forms.py
+++ Corey Schafer's Python in-depth tutorial/Flask Tutorial/flaskblog/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from matplotlib.pyplot import title
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 from flask_login import current_user
@@ -61,3 +62,9 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email = email.data).first()
             if user:
                 raise ValidationError('Email already exists! Choose another one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators = [DataRequired()])
+    content = TextAreaField('Content', validators = [DataRequired()])
+    submit = SubmitField('Post')
