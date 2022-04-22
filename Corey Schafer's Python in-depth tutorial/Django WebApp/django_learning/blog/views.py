b68673073faaa280f134a07498596abf62ebc673
Mushfiqur Rahman <u1604123@student.cuet.ac.bd>
2022-04-22 16:52:01 +0600

    Django WebApp part-3

--- Corey Schafer's Python in-depth tutorial/Django WebApp/django_learning/blog/views.py
+++ Corey Schafer's Python in-depth tutorial/Django WebApp/django_learning/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Author1',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 22, 2022'
+    },
+    {
+        'author': 'Author2',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 22, 2022'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Homepage</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
