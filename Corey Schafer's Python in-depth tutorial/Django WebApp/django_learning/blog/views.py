8a4d27d6a760fc8068306b4ecb8277b43debab4b
Mushfiqur Rahman <u1604123@student.cuet.ac.bd>
2022-04-22 20:43:56 +0600

    Full-Featured Django WebApp Part-5

--- Corey Schafer's Python in-depth tutorial/Django WebApp/django_learning/blog/views.py
+++ Corey Schafer's Python in-depth tutorial/Django WebApp/django_learning/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Author1',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 22, 2022'
-    },
-    {
-        'author': 'Author2',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 22, 2022'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
