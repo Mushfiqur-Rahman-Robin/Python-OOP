95ed5262a1243826a136eb449b1d8e2e95da23a3
Mushfiqur Rahman <u1604123@student.cuet.ac.bd>
2022-04-24 15:50:18 +0600

    Full-Featured Django WebApp Part-10

--- Corey Schafer's Python in-depth tutorial/Django WebApp/django_learning/blog/models.py
+++ Corey Schafer's Python in-depth tutorial/Django WebApp/django_learning/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -10,4 +11,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.title 
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk':self.pk})
