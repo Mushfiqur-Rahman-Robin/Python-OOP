748c311308e1a4ca4e4d312ed30b14cc502fad69
Mushfiqur Rahman <u1604123@student.cuet.ac.bd>
2022-04-24 20:01:27 +0600

    Full-Featured Django WebApp Part-12

--- Corey Schafer's Python in-depth tutorial/Django WebApp/django_learning/django_learning/settings.py
+++ Corey Schafer's Python in-depth tutorial/Django WebApp/django_learning/django_learning/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+from numpy import true_divide
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +136,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
