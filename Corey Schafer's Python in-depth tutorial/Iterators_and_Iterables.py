ea5551654ae681f21d314e776de1d32bf962b0e2
Mushfiqur Rahman <u1604123@student.cuet.ac.bd>
2022-04-25 11:50:33 +0600

    Iterators and Iterables

---
+++ Corey Schafer's Python in-depth tutorial/Iterators_and_Iterables.py
@@ -0,0 +1,37 @@
+nums = [1, 2, 3]
+
+i_nums = iter(nums)
+
+print(i_nums)
+print(next(i_nums))
+print(dir(i_nums))
+
+
+class MyRange:
+    def __init__(self, start, end):
+        self.value = start
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.value >= self.end:
+            raise StopIteration
+        current = self.value
+        self.value += 1
+        return current
+
+nums = MyRange(1,10)
+
+for num in nums:
+    print(num)
+
+print(next(nums))
+
+
+def my_range(start, end):
+    current = start
+    while current < end:
+        yield current
+        current += 1
